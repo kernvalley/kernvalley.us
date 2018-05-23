055b1fce51ab87903c8b62ecba7effaf8134820d
Chris Zuber <shgysk8zer0@gmail.com>
2018-05-23 12:00:30 -0700

    Add ShareAPI handler + shim

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 import './std-js/shims.js';
 import './std-js/deprefixer.js';
 import {loaded, $, wait, registerServiceWorker} from './std-js/functions.js';
+import webShareApi from './std-js/webShareApi.js';
+import {
+	facebook,
+	twitter,
+	googlePlus,
+	linkedIn,
+	reddit,
+	gmail,
+} from './std-js/share-config.js';
+
+webShareApi(facebook, twitter, googlePlus, linkedIn, reddit, gmail);
 
 loaded().then(async () => {
 	if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
