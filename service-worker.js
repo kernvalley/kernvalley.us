9f2599638886987482ad23d60c13f8e4708b21c0
Chris Zuber <shgysk8zer0@gmail.com>
2020-07-13 17:05:25 -0700

    Update CSP
    Various issues I'm working on.

--- service-worker.js
+++ service-worker.js
@@ -90,6 +90,7 @@ self.addEventListener('fetch', event => {
 self.addEventListener('push', event => {
 	const data = event.data.json();
 	if (('notification' in data) && Array.isArray(data.notification) && Notification.permission === 'granted') {
+
 		this.registration.showNotification(...data.notification);
 	}
 });
