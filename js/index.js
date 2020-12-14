762dc4679f9b3761a972ae894e5b33cfe87d400d
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-13 17:28:50 -0800

    Misc updates
    ### Added
    
    - Use `<button is="app-list">` component
    - Add `cookieStore` handling for theme
    
    ### Changed
    
    - Allow `apps.kernvalley.us` in CSP
    - Update style of nav links & buttons
    - Use weather component via `_includes/`
    - Move `<button is="pwa-install">` to `<nav>`

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ import 'https://cdn.kernvalley.us/components/github/user.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
 import 'https://cdn.kernvalley.us/components/pwa/install.js';
 import 'https://cdn.kernvalley.us/components/ad/block.js';
+import 'https://cdn.kernvalley.us/components/app/list-button.js';
+import { init } from 'https://cdn.kernvalley.us/js/std-js/data-handlers.js';
 import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 import { loadScript } from 'https://cdn.kernvalley.us/js/std-js/loader.js';
 import { importGa, externalHandler, telHandler, mailtoHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
@@ -27,8 +29,39 @@ if (typeof GA === 'string' && GA.length !== 0) {
 	});
 }
 
-loadScript('https://cdn.polyfill.io/v3/polyfill.min.js').catch(console.error);
+loadScript('https://cdn.polyfill.io/v3/polyfill.min.js').catch(console.error).finally(() => init());
 
-document.documentElement.classList.replace('no-js', 'js');
-document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
-document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+
+$(document.documentElement).toggleClass({
+	'no-dialog': document.createElement('dialog') instanceof HTMLUnknownElement,
+	'no-details': document.createElement('details') instanceof HTMLUnknownElement,
+	'js': true,
+	'no-js': false,
+});
+
+cookieStore.get({ name: 'theme' }).then(async cookie =>{
+	await $.ready;
+	const $ads = $('ad-block:not([theme]), ad-block[theme="auto"]');
+
+	const setTheme = async ({ name, value = 'auto' }) => {
+		if (name === 'theme') {
+			Promise.all([
+				$(':root, [data-theme]').data({ theme: value }),
+				$('[theme]:not(ad-block)').attr({ theme: value }),
+				$ads.attr({ theme: value }),
+			]);
+		}
+	};
+
+	if (cookie && typeof cookie.value === 'string') {
+		setTheme(cookie);
+	}
+
+	cookieStore.addEventListener('change', ({ changed, deleted }) => {
+		const cookie = [...changed, ...deleted].find(({ name }) => name === 'theme');
+
+		if (cookie) {
+			setTheme(cookie);
+		}
+	});
+});
