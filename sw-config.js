a2c1b47d428eeb3198e8d9f5c9f3c2cbedd53210
Chris Zuber <shgysk8zer0@gmail.com>
2021-05-18 19:35:01 -0700

    MIsc. fixes and updates
    Update layouts and JS

--- sw-config.js
+++ sw-config.js
@@ -2,37 +2,35 @@
 layout: null
 ---
 'use strict';
-/*eslint no-unused-vars: 0*/
+/* eslint-env serviceworker */
+/* eslint no-unused-vars: 0 */
 const config = {
 	version: '{{ site.data.app.version | default: site.version }}',
 	fresh: [
-		/* Root document */
-		'{{ site.pages | where: "pinned", true | map: "url" | join: "', '" }}',
-		/*'{{ site.posts | where: "pinned", true | map: "url" | join: "', '" }}',*/
+		'/',
 		'https://apps.kernvalley.us/apps.json',
 		'/manifest.json',
 	].map(path => new URL(path, location.origin).href),
 	stale: [
-		'/css/index.min.css',
-		'/js/index.min.js',
-		'/img/icons.svg',
 		/* Other HTML */
 		'https://cdn.kernvalley.us/components/toast-message.html',
 		'https://cdn.kernvalley.us/components/leaflet/map.html',
 		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.html',
 		'https://cdn.kernvalley.us/components/slide-show/slide-show.html',
+		'https://cdn.kernvalley.us/components/install/prompt.html',
 		'https://cdn.kernvalley.us/components/github/user.html',
 
-		/* JS, `customElements`, etc. */
+		/* JS */
+		'/js/index.min.js',
+
+		/* CSS */
+		'/css/index.min.css',
 		'https://cdn.kernvalley.us/components/toast-message.css',
 		'https://cdn.kernvalley.us/components/leaflet/map.css',
 		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.css',
 		'https://cdn.kernvalley.us/components/slide-show/slide-show.css',
 		'https://cdn.kernvalley.us/components/github/user.css',
-		'https://cdn.polyfill.io/v3/polyfill.min.js',
-		'https://cdn.kernvalley.us/components/pwa/install.js',
-
-		/* CSS */
+		'https://cdn.kernvalley.us/components/install/prompt.css',
 
 		/* Images & Icons */
 		'/img/apple-touch-icon.png',
@@ -40,6 +38,7 @@ const config = {
 		'/img/icon-192.png',
 		'/img/icon-32.png',
 		'/img/favicon.svg',
+		'/img/icons.svg',
 		'https://cdn.kernvalley.us/img/adwaita-icons/actions/mail-send.svg',
 		'https://cdn.kernvalley.us/img/adwaita-icons/actions/mark-location.svg',
 		'https://cdn.kernvalley.us/img/octicons/file-media.svg',
@@ -49,13 +48,15 @@ const config = {
 		/* Fonts */
 		'https://cdn.kernvalley.us/fonts/roboto.woff2',
 		'https://cdn.kernvalley.us/fonts/Libertine.woff',
-		/* Other */
 	].map(path => new URL(path, location.origin).href),
 	allowed: [
-		/https:\/\/maps\.wikimedia\.org\/osm-intl\/*/,
-		/https:\/\/secure\.gravatar\.com\/avatar\/*/,
-		/https:\/\/i\.imgur\.com\/*/,
-		/https:\/\/*\.githubusercontent\.com\/u\/*/,
-		/https:\/\/api\.github\.com\/users\/*/,
+		'https://secure/.gravatar/.com/avatar/',
+		'https://i.imgur.com',
+		'https://api.githubusercontent.com/us/',
+		'https://api.github.com/users/',
+		/\.(jpg|png|webp|svg|gif)$/,
 	],
+	alloweFresh: [
+		/\.(html|css|js|json)$/,
+	]
 };
