da9e861cf867759677ac6ce05c88def831477fa5
Chris Zuber <shgysk8zer0@gmail.com>
2023-05-15 12:45:58 -0700

    Update to node 18 & update config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-	"map": {inline: false},
-	"plugins": [
-		require("postcss-import"),
-		require("postcss-url"),
-		require('postcss-import-url'),
-		// require("postcss-cssnext"),
-		require("postcss-discard-comments"),
-		require("cssnano"),
+/* eslint-env node */
+import pcImport from 'postcss-import';
+import pcURL from 'postcss-url';
+import pcImportURL from 'postcss-import-url';
+import pcEnv from 'postcss-preset-env';
+import pcDiscardComments from 'postcss-discard-comments';
+import pcCustomProperties from 'postcss-custom-properties';
+import pcMediaMinMax from 'postcss-media-minmax';
+import CSSNano from 'cssnano';
+import postcssNesting  from 'postcss-nesting';
+export default {
+	map: {inline: false},
+	plugins: [
+		pcImport, pcURL,pcImportURL, pcEnv, pcDiscardComments, pcCustomProperties,
+		pcMediaMinMax, CSSNano, postcssNesting
 	]
 };
