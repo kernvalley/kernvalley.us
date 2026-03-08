a58ed4a618ad2597a1c8653cfd786106e031bc7b
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-08 15:50:02 -0700

    Update config, etc

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-/* eslint-env node */
 import { getConfig } from '@shgysk8zer0/js-utils/rollup';
 import { rollupImport, rollupImportMeta } from '@shgysk8zer0/rollup-import';
+import pkg from './package.json' with { type: 'json' };
 
 export default getConfig('./js/index.js', {
 	plugins: [
-		rollupImport('_data/importmap.yml'),
-		rollupImportMeta({ baseURL: 'https://kernvalley.us/' }),
+		rollupImport('_data/importmap.json'),
+		rollupImportMeta({ baseURL: pkg.homepage }),
 	],
 	format: 'iife',
 	minify: true,
